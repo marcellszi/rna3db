0e67414d16ef3b0c2fef7e089fb1da68cb8df8c3
Marcell Szikszai <64620912+marcellszi@users.noreply.github.com>
2025-12-08 08:25:41 +0800

    improved nohits detection

--- scripts/get_nohits.py
+++ scripts/get_nohits.py
@@ -11,17 +11,28 @@ if __name__ == "__main__":
     parser.add_argument("input_path", type=Path)
     parser.add_argument("output_path", type=Path)
     parser.add_argument("tbls_path", type=Path)
+    parser.add_argument("--e_value_threshold", type=float, default=1.0)
+    parser.add_argument("--length_threshold", type=int, default=64)
     args = parser.parse_args()
 
     all_chains, all_sequences = parse_fasta(args.input_path)
-    all_dict = {k: v for k, v in zip(all_chains, all_sequences)}
-    all_chains = set(all_chains)
-
     tbl = read_tbls_from_dir(args.tbls_path)
-    hit_chains = set(tbl.query_name)
 
-    nohits = all_chains - hit_chains
+    all_hits = set(tbl.query_name)
+    edge_hits = set(tbl.filter_e_value(args.e_value_threshold).query_name)
+    short_chains = set({k for k, v in zip(all_chains, all_sequences) if len(v) < args.length_threshold})
 
+    # chains that were not hit at all
+    zero_hits = set(all_chains) - all_hits
+
+    # chains that were not hit below the e-value threshold and are shorter than
+    # the length threshold
+    short_bad_hits = (set(all_chains) - edge_hits) & short_chains
+
+    # not hit at all or only bad hits
+    nohits = zero_hits | short_bad_hits
+
+    all_dict = dict(zip(all_chains, all_sequences))
     output_descriptions = list(nohits)
     output_sequences = [all_dict[i] for i in nohits]
 
